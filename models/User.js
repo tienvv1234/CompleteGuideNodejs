653f30fec86c60cdd38ccccdbf93fef32ccfff46
TienVu <tien.vuvan@launchdeckpowergate.com>
2018-09-08 02:07:56 +0700

    update

--- models/User.js
+++ models/User.js
@@ -1,14 +1,16 @@
 const mongoose = require('mongoose');
 
-// const { Schema } = mongoose; same here const Scheme = mongoose.Schema; completely equal --> es 2015 
+// const { Schema } = mongoose;
+// same here const Scheme = mongoose.Schema; completely equal --> es 2015
+
 const { Schema } = mongoose;
 
-//Schema có thể add thêm property mà không lo gì cả
+// Schema có thể add thêm property mà không lo gì cả
 const userSchema = new Schema({
-    googleId: String
+  googleId: String,
 });
 
 
-//mongoose.model('users'. userSchema); `note` 
-//- mongoose chỉ tạo khi nó không tồn tại nếu tồn tại rồi sẽ lấy ra
+// mongoose.model('users'. userSchema); `note`
+// - mongoose chỉ tạo khi nó không tồn tại nếu tồn tại rồi sẽ lấy ra
 mongoose.model('users', userSchema);
--- routes/plan.js
+++ routes/plan.js
@@ -1,62 +1,42 @@
-var stripe = require('stripe')('sk_test_euhBFP1reSeKIINuupZzw5KR');
-
-
-module.exports = (app) => {
-  app.get('/test', function (req, res) {
-    test().then(function (result) {
-      res.send({
-        status: "success",
-        token: result
-      });
-
-    }, function (error) {
-      res.send({
-        status: "error",
-        message: error.message
-      });
-
-    })
-  });
-
-}
+const stripe = require('stripe')('sk_test_euhBFP1reSeKIINuupZzw5KR');
 
 async function test() {
-  let plans = await stripe.plans.list();
-  //console.log("plans", plans);
-  console.log("plans[0].id", plans.data[0].id);
+  const plans = await stripe.plans.list();
+  // console.log("plans", plans);
+  console.log('plans[0].id', plans.data[0].id);
 
-  let cardToken = await stripe.tokens.create({
+  const cardToken = await stripe.tokens.create({
     card: {
       exp_month: 10,
       exp_year: 2018,
       number: '4242 4242 4242 4242',
-      cvc: 100
-    }
-  })
+      cvc: 100,
+    },
+  });
   // console.log("cardToken", cardToken)
-  console.log("cardToken.id", cardToken.id)
+  console.log('cardToken.id', cardToken.id);
 
-  let customerStripe = await stripe.customers.create({
+  const customerStripe = await stripe.customers.create({
     description: 'Customer for tienvv1234@gmail.com',
     email: 'tienvv1234@gmail.com',
-    source: cardToken.id // obtained with Stripe.js
-  })
+    source: cardToken.id, // obtained with Stripe.js
+  });
 
-  //console.log("customerStripe", customerStripe)
+  // console.log("customerStripe", customerStripe)
 
-  console.log("customerStripe", customerStripe.id)
+  console.log('customerStripe', customerStripe.id);
 
 
-  let subscription = await stripe.subscriptions.create({
+  const subscription = await stripe.subscriptions.create({
     customer: customerStripe.id,
     items: [
       {
         plan: plans.data[0].id,
       },
-    ]
+    ],
   });
 
- console.log("subscription", subscription);
+  console.log('subscription', subscription);
 
   // stripe.tokens.create({
   //   card: {
@@ -76,9 +56,6 @@ async function test() {
   //     console.log("err", customer);
   //   });
   // });
-
-
-
   // stripe.subscriptions.create({
   //   customer: "cus_DEYf08f0Jkyvqz",
   //   items: [
@@ -103,3 +80,18 @@ async function test() {
 }
 
 
+module.exports = (app) => {
+  app.get('/test', (req, res) => {
+    test().then((result) => {
+      res.send({
+        status: 'success',
+        token: result,
+      });
+    }, (error) => {
+      res.send({
+        status: 'error',
+        message: error.message,
+      });
+    });
+  });
+};
