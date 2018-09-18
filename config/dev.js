2d352775bab9059ee989b59d325853a33fe984c5
TienVu <tien.vuvan@launchdeckpowergate.com>
2018-09-18 23:20:43 +0700

    update infomation

---
+++ config/dev.js
@@ -0,0 +1,8 @@
+// dev.js - don't commit this !!!
+
+module.exports = {
+  googleClientID: '882894736716-e2vunmfaagrq25cnrjue7q8oalt5ltr7.apps.googleusercontent.com',
+  googleClientSecret: 'GvHeg-NbesCpniiNhRDFK099',
+  mongoUri: 'mongodb://admin:tien1991@ds161610.mlab.com:61610/email-dev-tienvv',
+  cookieKey: 'sdlkjflsdjflsjdflnicvpawermlawmczcxozvw',
+};
